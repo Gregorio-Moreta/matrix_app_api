29da7ce08bd45e1e2bd08e709e5c41940d60e0cf
Gregorio Moreta <gregoriomoreta4@gmail.com>
2021-12-11 16:46:58 -0500

    reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 
 Character.create([
-	{ name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+	{ name: "Neo", description: "Neo (born as Thomas A. Anderson, also known as The One, an anagram for Neo) is a fictional character and the protagonist of The Matrix franchise, created by the Wachowskis. He was portrayed as a cybercriminal and computer programmer by Keanu Reeves in the films, as well as having a cameo in The Animatrix short film Kid's Story. Andrew Bowen provided Neo's voice in The Matrix: Path of Neo.[1] In 2021, Reeves will reprise his role in the upcoming film The Matrix Resurrections[2] with what Vulture calls " },
+	{ name: "Trinity", description: "Morpheus (/ˈmɔːrfiəs/) is a fictional character in The Matrix franchise.[2] He is portrayed by Laurence Fishburne in the first three films, and in the video game The Matrix: Path of Neo, where his original actor was the only one to reprise his character's voice.[3] In The Matrix Resurrections, he is portrayed by Yahya Abdul-Mateen II.[1][4][5]" },
+	{ name: "Morpheus", description: "Trinity is a fictional character in the Matrix franchise.[1] She is portrayed by Carrie-Anne Moss in the films. In the gameplay segments of Path of Neo, she is voiced by Jennifer Hale. Trinity first appears in the first film in the trilogy, The Matrix." },
+	{ name: "Agent Smith", description: "Agent Smith is a fictional character and the main antagonist of The Matrix franchise. He was portrayed by Hugo Weaving in the first trilogy of films and voiced by Christopher Corey Smith in The Matrix: Path of Neo. He also makes a cameo appearance in the anime film The Animatrix, voiced by Matt McKenzie. Jonathan Groff portrays Smith in The Matrix Resurrections.[1]" },
 ])
 
 
